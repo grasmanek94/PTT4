af5764b0d7f2a94f00371a71a91325af59bcadfb
Threedexter <mathieuvwunnik@planet.nl>
2016-05-11 12:43:34 +0200

    Created server application
    With WCF and dataclasses for data storage and connectivity to WCF
    client, and basic Serial interface

---
+++ KnikkerBaanServer/KnikkerBaanServer/IScadaService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace KnikkerBaanServer
+{
+    [ServiceContract]
+    public interface IScadaService
+    {
+        [OperationContract]
+        void SetPolicy(Policy policy);
+
+        [OperationContract]
+        List<CanMessage> GetCanMessages();
+
+        [OperationContract]
+        Policy GetPolicy();
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "KnikkerBaanServer.ContractType".
+    [DataContract]
+    public class Policy
+    {
+       //ToDo: Determine Properties
+    }
+}
