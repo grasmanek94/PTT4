988b5483aa0fdfa0aa25081412a7ca359eec38d5
Minh-Triet Diep <minhtrietdiep@gmail.com>
2016-06-08 12:26:03 +0200

    Integrate CAN code into IR code
    It compiles!
    
    Untested and not fully implemented

--- Arduino/Infrared_Sensor_CAN/Infrared_Sensor_CAN.ino
+++ Arduino/Infrared_Sensor_CAN/Infrared_Sensor_CAN.ino
@@ -1,7 +1,16 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <MCP2515.h>
+
 #define ArraySize 10
 #define ServoPin 3
 #define SensorPin A0
-#include <Servo.h>
+
+/*
+ * Defines for CAN addresses
+ */
+#define CANSERVER 0xAA
+#define IRMODULE  0xBB
 
 int Average = 0;
 bool IsInitialised = false;
@@ -14,6 +23,11 @@ int small_marble_size = 680;
 int large_marble_size = 580;
 int Threshold = 6300;
 int Default_value = 6400;
+
+CANMSG msgReceive;
+MCP2515 can;
+
+
 
 enum State
 {
@@ -34,6 +48,20 @@ void setup()
   myServo.write(servo_rest);
   Blink();
   delay(200);
+  
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  
+  if(can.initCAN(CAN_BAUD_100K) == 0)
+  {
+    Serial.println("initCAN() failed");
+    while(1);
+  }
+  
+  if(can.setCANNormalMode(LOW) == 0) //normal mode non single shot
+  {
+    Serial.println("setCANNormalMode() failed");
+    while(1);
+  }
 }
 
 void loop() {
@@ -43,6 +71,12 @@ void loop() {
   if (allow == 0)
   {
     MovingAverage();
+  }
+  handleCAN(can.receiveCANMessage(&msgReceive, 100));
+  if (allow) {
+    int msgLength = 2;
+    byte msgData[2] = {0xBE, 0xEF};
+    sendCanMsg(IRMODULE, msgLength, msgData);
   }
 }
 
@@ -144,4 +178,73 @@ void Blink()
   delay(200);
 }
 
+void handleCAN(bool messageReceived) {
+  //msgReceive ipv msg
+  //int i = can.receiveCANMessage(&msg, 100);
+
+  msgReceive.dataLength;
+  msgReceive.adrsValue;
+
+  // We received a thing from the server, this is probably
+  if (msgReceive.adrsValue == CANSERVER) {
+    // Do something with msgReceive.data[]
+  }
+  
+  /*
+  int data = msg.data[2];
+  if (msg.adrsValue == 0xFF) {
+    digitalWrite(led2, HIGH);
+    Serial.print(">0xFF: ");
+    Serial.print(data);
+    Serial.print(";\n");
+  }
+  else {
+    digitalWrite(led2, LOW);
+  }
+  if (msg.adrsValue == 0xDD) {
+    digitalWrite(led3, HIGH);
+    Serial.print(">0xDD: ");
+    Serial.print(data);
+    Serial.print(";\n");
+  }
+  else {
+    digitalWrite(led3, LOW);
+  }
+  if(msg.data[2] == 123) {
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    delay(100);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    delay(100);
+    Serial.println("data received");
+  }
+  */
+}
+
+void sendCanMsg(unsigned long adrsValue, int dataLength, byte *data) {
+  CANMSG msg;
+  msg.adrsValue = adrsValue;
+  msg.isExtendedAdrs = false;
+  msg.rtr = false;
+  msg.dataLength = dataLength;
+  for (int i = 0; i < dataLength; i++) {
+    msg.data[i] = data[i];
+  }
+  can.transmitCANMessage(msg, 1000);
+  /*msg.adrsValue = 0x7df;
+  msg.isExtendedAdrs = false;
+  msg.rtr = false;
+  msg.dataLength = 8;
+  msg.data[0] = 0x02;
+  msg.data[1] = 0x01;
+  msg.data[2] = 123;
+  msg.data[3] = 0;
+  msg.data[4] = 0;
+  msg.data[5] = 0;
+  msg.data[6] = 0;
+  msg.data[7] = 0;
+  can.transmitCANMessage(msg, 1000);
+  Serial.println("data transmitted");*/
+}
 
