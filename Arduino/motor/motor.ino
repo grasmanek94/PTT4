9f5ee91dda6ac810c85bbf6c623376c30cf9ef0b
Thomas <t.schraven@student.fontys.nl>
2016-06-08 17:05:26 +0200

    Get time between pulses from encoder

--- Arduino/motor/motor.ino
+++ Arduino/motor/motor.ino
@@ -1,15 +1,40 @@
 #include "motorshield.h"
 
+#define INT_PIN 3
+
 DualVNH5019MotorShield motorController;
+
+volatile long prevTime;
+volatile long currTime;
+
+long lastTime;
 
 void setup() 
 {
-  pinMode(7, INPUT_PULLUP);
+  pinMode(INT_PIN, INPUT_PULLUP);
+  motorController.init();
   motorController.setSpeeds(400,400);
+
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), isr, CHANGE);
+
+  Serial.begin(9600);
+
+  lastTime = millis();
 }
 
 void loop() 
 {
+  if ((millis() - lastTime) > 500)
+  {
+      int delta = currTime - prevTime;
+      Serial.println(delta);
+      lastTime = millis();
+  }
+}
 
+void isr()
+{
+  prevTime = currTime;
+  currTime = micros();
 }
 
