c4b4284a008483c5f563c2278fa1ede013c7d0ab
Gamer_Z <grasmanek94@gmail.com>
2016-06-08 13:02:23 +0200

    fix pin not moving

--- Arduino/sweeper/sweeper.ino
+++ Arduino/sweeper/sweeper.ino
@@ -11,6 +11,7 @@ class MarblePassThrough
     Servo servo;
     bool move_state;
     long move_time;
+    int servo_pin;
         
     int detector_pin;
     long last_detect_time;
@@ -18,17 +19,13 @@ class MarblePassThrough
 public:
     MarblePassThrough(int servoPin, int detectorPin)
     {
-        //pinMode(detectorPin, INPUT_PULLUP);   
-        servo.attach(servoPin);
-        
-        move_state = true;
         detector_pin = detectorPin;
+        servo_pin = servoPin;
+        move_state = true;      
         move_time = 5000;
         detect_time = 1000;
         last_detect_time = 0;   
-        pos_now = 0;
-             
-        Begin();
+        pos_now = 0;        
     }
 
     ~MarblePassThrough()
@@ -36,21 +33,26 @@ public:
         
     }
 
+    void Setup()
+    {
+        pinMode(detector_pin, INPUT_PULLUP);   
+        servo.attach(servo_pin);
+             
+        Begin();
+    }
+    
     bool Tick()
     {
-        servo.write(100);
-        delay(5000);
         long time_now = millis();
         if(time_now >= time_start && time_now <= time_end)
         {
             long time_diff = time_now - time_start;
             float percentage = ((float)time_diff) / ((float)(move_time));
             int pos_calculated = pos_start + (int)(((float)(pos_end - pos_start)) * percentage);
-            if(abs(pos_calculated - pos_now) >= 10)
+            if(pos_calculated != pos_now)
             {
                 pos_now = pos_calculated;
-                servo.write(100);
-                delay(50);
+                servo.write(pos_now);
                 Serial.println(pos_now);
             }
             
@@ -107,7 +109,7 @@ public:
         }
         
         bool ballPassed = digitalRead(detector_pin);        
-        if(!ballPassed)
+        if(ballPassed)
         {
             return false;
         }
@@ -124,6 +126,7 @@ void setup()
 {
     Serial.begin(9600);
     pinMode(13, OUTPUT);
+    passer.Setup();
 }
 
 void loop() 
