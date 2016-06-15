a468a50a44bead5c3ddf2798a5bd37eca373694f
Gamer_Z <grasmanek94@gmail.com>
2016-06-15 13:05:15 +0200

    standarize CAN for RGB sensor

---
+++ GESTANDARIZEERD/KnikkerDoorlaatModule/KnikkerDoorlaatModule.ino
@@ -0,0 +1,222 @@
+#include <Servo.h>
+#include <Arduino.h>
+#include <MCP2515.h>
+#include "canx.h"
+
+class MarblePassThrough
+{
+    long time_start;
+    long time_end;
+    int pos_start;
+    int pos_now;
+    int pos_end;
+    Servo servo;
+    bool move_state;
+    long move_time;
+    int servo_pin;
+        
+    int detector_pin;
+    long last_detect_time;
+    long detect_time;
+
+    bool getting_marble;
+
+    bool CheckPosition()
+    {
+         long time_now = millis();
+        if(time_now >= time_start && time_now <= time_end)
+        {
+            long time_diff = time_now - time_start;
+            float percentage = ((float)time_diff) / ((float)(move_time));
+            int pos_calculated = pos_start + (int)(((float)(pos_end - pos_start)) * percentage);
+            if(pos_calculated != pos_now)
+            {
+                pos_now = pos_calculated;
+                servo.write(pos_now);
+            }
+            
+            return false;
+        }
+        else
+        {
+            servo.write(pos_end);
+            return true;
+        }       
+    }
+
+    void PassMarble()
+    {
+        pos_start = 107;
+        pos_end = 0;
+        time_start = millis();
+        time_end = time_start + move_time;
+        move_state = false;
+    }
+
+    void LoadMarble()
+    {
+        pos_start = 0;
+        pos_end = 107;   
+        time_start = millis();
+        time_end = time_start + move_time;      
+        move_state = true;       
+    }
+
+    void Switch()
+    {
+        if(!move_state)
+        {
+            LoadMarble();
+        }
+        else
+        {
+            PassMarble();
+        }
+    }
+    
+    bool GetMoveState()
+    {
+        return move_state;
+    }
+
+    bool DetectedMarble()
+    {
+        long time_now = millis();
+        if(time_now < (last_detect_time + detect_time))
+        {
+            return false;
+        }
+        
+        bool ballPassed = digitalRead(detector_pin);        
+        if(ballPassed)
+        {
+            return false;
+        }
+        
+        last_detect_time = time_now;
+        return true;       
+    }
+          
+public:
+    MarblePassThrough(int servoPin, int detectorPin)
+    {
+        detector_pin = detectorPin;
+        servo_pin = servoPin;
+        move_state = true;      
+        move_time = 1000;
+        detect_time = 1000;
+        last_detect_time = 0;   
+        pos_now = 0;    
+        getting_marble = false;    
+    }
+
+    ~MarblePassThrough()
+    {
+        
+    }
+
+    void Setup()
+    {
+        pinMode(detector_pin, INPUT_PULLUP);   
+        servo.attach(servo_pin);
+             
+        LoadMarble();
+    }
+
+    bool GettingMarbleTick()
+    {
+        if(getting_marble)
+        {
+            if(CheckPosition())
+            {
+                Switch();
+            }
+
+            if(DetectedMarble())
+            {
+                StopMarble();
+                return true;
+            }            
+        }
+        return false;
+    }
+
+    void GetMarble()
+    {
+        if(!getting_marble)
+        {
+            getting_marble = true;
+            transmitCAN(messageLiftStart);
+        }
+    }
+
+    void StopMarble()
+    {
+        if(getting_marble)
+        {
+            getting_marble = false;
+            transmitCAN(messageLiftStop);
+        }
+    }
+
+    bool GettingMarble()
+    {
+        return getting_marble;
+    }
+};
+
+MarblePassThrough passer(4, 2);
+bool lightning = false;
+
+bool ProcessIncommingMessages()
+{
+    CANMSG canReceived;
+    CustomCanMessage smsg;
+    if (can.receiveCANMessage(&canReceived, CAN_MS_TIMEOUT) && ParseMessage(canReceived, smsg))
+    {
+        if (smsg.receiverAddress == CAN_MyAddress)
+        {
+            passer.GetMarble();
+        }
+        else if (smsg.receiverAddress == CAN_Address_Broadcast)
+        {
+            switch(smsg.function)
+            {
+                case BROADCAST_MARBLE_ACCEPTED:
+                    if(smsg.senderAddress == CAN_Address_Transparency)
+                    {
+                        passer.GetMarble();
+                    }
+                break;
+                case BROADCAST_MARBLE_REJECTED:
+                    passer.GetMarble();
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("SWEEPER ONLINE");
+    pinMode(13, OUTPUT);
+    InitCan();
+    
+    passer.Setup();
+}
+
+void loop() 
+{
+    if(passer.GettingMarbleTick())
+    {
+        lightning ^= 1;
+        digitalWrite(13, lightning);
+        Serial.println("DM");
+        transmitCAN(messageBroadcastMarblePassed);
+    }
+
+    ProcessIncommingMessages();
+}
+
---
+++ GESTANDARIZEERD/RGBsensor/RGBsensor.ino
@@ -0,0 +1,130 @@
+#include <ADJDS311.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <MCP2515.h>
+#include <Servo.h>
+#include "canx.h"
+
+#define doorstand 30
+#define beginstand 80
+#define wegstand 180
+#define GROEN 1
+#define WIT   2
+//#define ZWART 3
+Servo myservo;  // create servo object to control a servo
+int sensorLed_pin = 2; //LED on the ADJDS-311
+int pos = 0;    // variable to store the servo position
+int mem[] = {0, 0, 0};
+int newmem[] = {0, 0, 0};
+int kleurkeuze = WIT;
+ADJDS311 colorSensor(sensorLed_pin);
+CustomCanMessage can_message;
+CANMSG msg;
+
+// SDA - 4
+// SCL - 5
+void setup() 
+{
+    Serial.begin(9600);
+    SPI.setClockDivider(SPI_CLOCK_DIV8);
+    
+    InitCan();
+    
+    pinMode(sensorLed_pin, OUTPUT);
+    colorSensor.init();
+    colorSensor.ledOn(); //turn LED on
+    
+    myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+    myservo.write(beginstand);
+    delay(2000);
+    //Calibrate white
+    //Need to hold white card in front (1-3mm) of it to calibrate from
+    colorSensor.calibrate();
+    Serial.println("remove");
+    delay(2000);
+    RGBC begincolor = colorSensor.read(); //read the color
+    mem[0] = begincolor.red;
+    mem[1] = begincolor.green;
+    mem[2] = begincolor.blue;
+}
+
+void loop() 
+{
+    RGBC color = colorSensor.read(); //read the color
+
+    if (can.receiveCANMessage(&msg, 1000) && ParseMessage(msg, can_message))
+    {
+        if(can_message.receiverAddress = CAN_MyAddress)
+        {
+            switch (msg.data[2])
+            {
+            case 1:
+                //kleurkeuze =
+                break;
+            case 2:
+                //kleurkeuze =
+                break;
+            case 3:
+                //kleurkeuze =
+                break;
+            }
+            readRGB(color);
+            delay(100);
+        }
+        else if(can_message.receiverAddress = CAN_Address_Broadcast)
+        {
+            //received broadcast message
+        }
+    }
+}
+
+void readRGB(RGBC color) 
+{
+    //RGBC is an array of red/green/blue/clear readings
+    //Take a RGBC, and try to reproduce it on an RGB LED
+    //This does not work very well as is because of how colors/our eyes work
+    newmem[0] = map(color.red, 0, 1024, 0, 255);
+    Serial.print("R");
+    Serial.print(newmem[0]);
+    newmem[1] = map(color.green, 0, 1024, 0, 255);
+    Serial.print(" G");
+    Serial.print(newmem[1]);
+    newmem[2] = map(color.blue, 0, 1024, 0, 255);
+    Serial.print(" B");
+    Serial.println(newmem[2]);
+    
+    if ((newmem[0] - mem[0]) > 6 || (newmem[0] - mem[0]) < -6)
+    {
+        delay(500);
+        color = colorSensor.read(); //read the color
+        newmem[0] = map(color.red, 0, 1024, 0, 255);
+        newmem[1] = map(color.green, 0, 1024, 0, 255);
+        newmem[2] = map(color.blue, 0, 1024, 0, 255);
+
+        //case WIT:
+        if (newmem[0] > 200 && newmem[1] > 200 && newmem[2] > 180)
+        {
+            myservo.write(wegstand);
+            delay(500);
+            myservo.write(beginstand);
+            transmitCAN(messageRejected);
+        }
+        else
+        {
+            myservo.write(doorstand);
+            delay(1000);
+            myservo.write(beginstand);
+            transmitCAN(messagePassed);
+        }
+        delay(400);
+        color = colorSensor.read(); //read the color
+        newmem[0] = map(color.red, 0, 1024, 0, 255);
+        newmem[1] = map(color.green, 0, 1024, 0, 255);
+        newmem[2] = map(color.blue, 0, 1024, 0, 255);
+    }
+    
+    mem[0] = newmem[0];
+    mem[1] = newmem[1];
+    mem[2] = newmem[2];
+    delay(400);
+}
